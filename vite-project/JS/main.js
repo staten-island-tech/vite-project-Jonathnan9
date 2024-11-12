a30875454be4fa99b882544ddd3d89a29185308e
Jonathan <158211587+Jonathan9123@users.noreply.github.com>
2024-11-12 02:46:47 -0500

    b

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -1,22 +1,45 @@
 import "../CSS/style.css";
+import "../CSS/variables.css";
 import { characters } from "./items.js";
 
 const DOMSelectors = {
   themeButtons: document.querySelectorAll(".theme-button"),
+  sortOptions: document.getElementById("sort-options"),
+  boxesContainer: document.querySelector(".boxes"),
 };
 
-function createCharacterCard(name, type, gameVersions, imageUrl) {
+function createCharacterCard(
+  name,
+  type,
+  gameVersions,
+  imageUrl,
+  plantType,
+  sunCost,
+  world,
+  rarity,
+  damageType,
+  specialAbility
+) {
   const container = document.querySelector(".boxes");
 
   container.insertAdjacentHTML(
     "beforeend",
     `
-    <div class="box">
-      <h2>${name}</h2>
-      <img src="${imageUrl}" alt="${name}" />
-      <p>Type: ${type}</p>
-      <p>Game Versions: ${gameVersions.join(", ")}</p>
-    </div>
+      <div class="box">
+        <h2>${name}</h2>
+        <div class="content">
+        <img src="${imageUrl}" alt="${name}" />
+        <div class="details">
+          <p>Type: ${type}</p>
+          <p>Game Versions: ${gameVersions.join(", ")}</p>
+          <p>Plant Type: ${plantType}</p>
+          <p>Sun Cost: ${sunCost}</p>
+          <p>World: ${world}</p>
+          <p>Rarity: ${rarity}</p>
+          <p>Damage Type: ${damageType}</p>
+          <p>Special Ability: ${specialAbility}</p>
+        </div>
+      </div>
     `
   );
 }
@@ -27,7 +50,13 @@ function displayCharacters() {
       character.characterName,
       character.characterType,
       character.gameVersions,
-      character.Link
+      character.Link,
+      character.plantType,
+      character.sunCost,
+      character.world,
+      character.rarity,
+      character.damageType,
+      character.specialAbility
     );
   });
 }
